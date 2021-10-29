d3795076ceaeb30d2fb481d82c1c24f6a720fb2e
kplacido <cleytonplacido@gmail.com>
2021-10-29 20:53:25 -0300

    Implantação do Grid

--- src/main/java/Core/DriverFactory.java
+++ src/main/java/Core/DriverFactory.java
@@ -2,6 +2,11 @@ package Core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -22,11 +27,25 @@ public class DriverFactory {
 
     public static WebDriver initDriver() {
         WebDriver driver = null;
-        if(driver == null) {
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            return driver;
+
+        if(Propriedades.TIPO_EXECUCAO == "local") {
+
+            if (driver == null) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                return driver;
+            }
+        }
+        if(Propriedades.TIPO_EXECUCAO == "Grid") {
+            DesiredCapabilities cap;
+            cap = DesiredCapabilities.chrome();
+            try {
+                driver = new RemoteWebDriver(new URL(" http://192.168.0.115:4444/wd/hub"),cap);
+            } catch (MalformedURLException e) {
+                System.err.println("falha na conexão com o GRID");
+                e.printStackTrace();
+            }
         }
         return driver;
     }
--- src/main/java/Core/Propriedades.java
+++ src/main/java/Core/Propriedades.java
@@ -3,4 +3,5 @@ package Core;
 public class Propriedades {
 
     public static boolean FECHAR_BROWSER = false;
+    public static String TIPO_EXECUCAO = "Grid";
 }
